289c9ed6de1d55357af8d17a6f847d4c3e7f9ae9
ralph kemp <ralphk3mp@gmail.com>
2018-05-09 20:24:50 +0100

    added elint

--- src/components/bookmark_list.js
+++ src/components/bookmark_list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import BookmarkItems from './bookmark_items'
+import BookmarkItems from './bookmark_items';
+
 
 class BookmarkList extends Component {
   constructor(props) {
