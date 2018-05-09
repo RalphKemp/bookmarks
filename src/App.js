97fd16d191a4522031c25dddd8b76a347d56243b
ralph kemp <ralphk3mp@gmail.com>
2018-05-09 18:00:24 +0100

    console log shows bookmark submissions currently working, going to create new component for actual bookmark

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoList from './components/todo_list';
+import BookmarkList from './components/bookmark_list';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <BookmarkList />
       </div>
     );
   }
---
+++ src/components/bookmark_list.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class BookmarkList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+    console.log(this.state.text);
+  }
+
+
+  render() {
+    return (
+      <div className="bookmark-list-main-container">
+        <div className="header">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              placeholder="Enter bookmark URL"
+              value={this.state.text}
+              onChange={this.handleChange} >
+            </input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookmarkList;
