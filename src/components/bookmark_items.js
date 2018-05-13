56450bbef8befac086ed9084daa586e73bc8dee3
ralph kemp <ralphk3mp@gmail.com>
2018-05-13 23:13:39 +0100

    working on form list css, matching the style of the form. Will move into more responsive design when fininshed the basic mobile view

--- src/components/bookmark_items.js
+++ src/components/bookmark_items.js
@@ -6,7 +6,7 @@ class BookmarkItems extends Component {
       <ul className="bookmark-items">
         {this.props.items.map((item, index) => {
           return (
-          <div>
+          <div className="list-item">
             <li key={item.id}><a href={"http://"+item.text}>{item.text}</a></li>
             <button
               onClick={(e) => {this.props.removeItem(index)}}
