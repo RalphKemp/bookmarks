3216a41f1d4661735477b5b8ee9620850553c151
ralph kemp <ralphk3mp@gmail.com>
2018-05-14 14:14:56 +0100

    changed regex but still not working as desired, will fix

--- src/components/bookmark_list.js
+++ src/components/bookmark_list.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import BookmarkItems from './bookmark_items';
 import swal from 'sweetalert';
-import isValidDomain from 'is-valid-domain';
 
 class BookmarkList extends Component {
   constructor(props) {
@@ -29,8 +28,13 @@ class BookmarkList extends Component {
     }
   }
 
-
   handleChange({target: {value}}) {
+
+    function isValidDomain(domain) {
+      const regex = new RegExp(/^@(([a-zA-Z](-?[a-zA-Z0-9])*)\.)*[a-zA-Z](-?[a-zA-Z0-9])+\.[a-zA-Z]{2,}$/);
+      return domain.match(regex);
+    }
+
     this.setState({
       text: value,
       valid: isValidDomain(value)
