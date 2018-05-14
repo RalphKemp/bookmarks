479b50a138ddce92e68bb5868b06b4a6c2a7de67
ralph kemp <ralphk3mp@gmail.com>
2018-05-15 00:07:59 +0100

    still working on getting the edit working, ran into some problems, will fix tomorrow evening

--- src/components/bookmark_items.js
+++ src/components/bookmark_items.js
@@ -7,33 +7,48 @@ class BookmarkItems extends Component {
       isEdit: false
     };
     this.editItem = this.editItem.bind(this);
+    this.renderForm = this.renderForm.bind(this);
   }
 
-   editItem() {
+  editItem() {
     this.setState({
       isEdit: !this.state.isEdit
     });
   }
 
+  renderForm() {
+    return (
+      <form>
+        <input></input>
+        <button>ok</button>
+      </form>
+    )
+  }
+
   render(){
+
+    const {isEdit} = this.state;
+
+    const test = isEdit ? (<div>yes</div>) : (<div>no</div>);
+
     return (
       <ul className="bookmark-items">
         {this.props.items.map((item, index) => {
-          console.log(this.state.isEdit);
           return (
-          <div className="list-item">
-            <li className="list-item-li" key={item.id}><a href={"http://"+item.text}>{item.text}</a></li>
-            <button
-              onClick={(e) => {this.props.removeItem(index)}}
-              className="remove-button">x
-            </button>
-            <button
-              onClick={(e) => {this.editItem(index)}}
-              className="edit-button"><i className="fa fa-pencil"></i>
-            </button>
-          </div>
-          )
-        })}
+            <div className="list-item">
+              <li className="list-item-li" key={item.id}><a href={"http://"+item.text}>{item.text}</a></li>
+              <button
+                onClick={(e) => {this.props.removeItem(index)}}
+                className="remove-button">x
+              </button>
+              <button
+                onClick={(e) => {this.editItem(index)}}
+                className="edit-button"><i className="fa fa-pencil"></i>
+              </button>
+            </div>
+            );
+          }
+        )}
       </ul>
     );
   }
